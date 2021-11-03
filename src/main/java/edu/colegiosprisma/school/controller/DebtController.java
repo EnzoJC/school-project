e14389c6cb6b2ded5878eb079b7276162a70f9dc
EnzoJC <enz10.cr@gmail.com>
2021-11-03 02:14:42 -0500

    Agregando correcciones

--- src/main/java/edu/colegiosprisma/school/controller/DebtController.java
+++ src/main/java/edu/colegiosprisma/school/controller/DebtController.java
@@ -2,8 +2,7 @@ package edu.colegiosprisma.school.controller;
 
 import edu.colegiosprisma.school.service.IDebtService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,15 +15,10 @@ public class DebtController {
         this.debtService = debtService;
     }
 
-    @GetMapping("parent/admision/pay")
-    public String pagar(@RequestParam("idStudent") String idStudent, Model model) {
-        model.addAttribute("idStudent", idStudent);
-        return "modalPay";
-    }
-
     @PostMapping("parent/admision/pay")
-    public String pagar(@RequestParam("idStudent") String idStudent, @RequestParam("idPayment") String idPayment) {
-
+    public String pagarMatricula(@RequestParam("idStudent") String idStudent,
+                        @RequestParam(value = "idPayment", required = false, defaultValue = "11") String idPayment) {
+        System.out.println("Hola: " + idStudent);
         return "redirect:/parent";
     }
 }
